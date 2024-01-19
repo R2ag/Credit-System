e20295a55385aa9d75e9de0f8564d6a21f9d5965
Rafael <rlag@outlook.com>
2024-01-18 21:28:47 -0300

    Startup Project

---
+++ src/main/kotlin/com/r2ag/creditsystem/CreditSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.r2ag.creditsystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CreditSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<CreditSystemApplication>(*args)
+}
---
+++ src/test/kotlin/com/r2ag/creditsystem/CreditSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.r2ag.creditsystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CreditSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
